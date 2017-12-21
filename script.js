842f56d173ca888716d5e2f007eb72f211a4f466
short-tale <34485752+short-tale@users.noreply.github.com>
2017-12-21 11:06:44 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 alert("Հարգելի օգտվող ուզում ենք զգուշացնել, որ միանալու է երաժշտություն։ Եթե ձեզ դա կանհանգստացնի կարող եք իջացնել համակարգչի ձայնը");
 var audio = new Audio('horse.mp3');
-function test1(){
-	var answer = prompt("Որ ճանապարհով գնամ, որ հասնեմ նվերին(1,2,3,4,5,6)");
-	if(answer=="5"){
-		alert("Այո, Ճիշտ է:) \n Կարող ես անցնել հաջորդ խնդրին");
-	}
-	else{
-		alert("Ոչ, Սխալ է :( \n կրկին փորձիր");
-	}
+function back_audio(){
+	audio.play();
+	setTimeout(back_audio, 20000);
 }
+back_audio();
 function test2(){
 	var answer = prompt("Քանի խաղալիք կարող ենք կախել ծառին(0,1,2,3,4)");
 	if(answer=="3"){
@@ -18,11 +14,15 @@ function test2(){
 		alert("Ոչ, Սխալ է :( \n կրկին փորձիր");
 	}
 }
-function back_audio(){
-	audio.play();
-	setTimeout(back_audio, 20000);
+function test1(){
+	var answer = prompt("Որ ճանապարհով գնամ, որ հասնեմ նվերին(1,2,3,4,5,6)");
+	if(answer=="5"){
+		alert("Այո, Ճիշտ է:) \n Կարող ես անցնել հաջորդ խնդրին");
+	}
+	else{
+		alert("Ոչ, Սխալ է :( \n կրկին փորձիր");
+	}
 }
-back_audio();
 function tale4(){
 	var heavy = prompt("Գրեք մի ծանր բան");
 	var hate = prompt("Մի մարդ, որին տանել չեք կարողանում");
